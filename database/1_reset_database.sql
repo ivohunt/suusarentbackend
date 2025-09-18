60278429810bd683a12c15bec6d08c42411da15d
ivohunt <ivo.hunt@gmail.com>
2025-09-18 13:23:17 +0300

    Infra

--- database/1_reset_database.sql
+++ database/1_reset_database.sql
@@ -1,7 +1,7 @@
 -- Kustutab public schema (mis põhimõtteliselt kustutab kõik tabelid)
 DROP SCHEMA IF EXISTS suusarent CASCADE;
 -- Loob uue public schema vajalikud õigused
-CREATE SCHEMA suusarent
+CREATE SCHEMA suusarent;
 -- taastab vajalikud andmebaasi õigused
     GRANT ALL ON SCHEMA suusarent TO postgres;
 GRANT ALL ON SCHEMA suusarent TO PUBLIC;
