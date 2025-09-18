6f9035356565005f1c2f488f5defa5a63daeddb6
ivohunt <ivo.hunt@gmail.com>
2025-09-18 11:20:03 +0300

    Initial commit

---
+++ src/main/java/com/suusarent/suusarentback/SuusarentbackApplication.java
@@ -0,0 +1,13 @@
+package com.suusarent.suusarentback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuusarentbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuusarentbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suusarent/suusarentback/SuusarentbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suusarent.suusarentback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuusarentbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
