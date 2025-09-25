a077a5668b28b3950714271d76f385ca3c48b0ba
ivohunt <ivo.hunt@gmail.com>
2025-09-25 16:27:00 +0300

    Kategooria backend

--- src/main/java/com/suusarent/suusarentback/controller/login/dto/LoginResponse.java
+++ src/main/java/com/suusarent/suusarentback/controller/login/dto/LoginResponse.java
@@ -6,12 +6,4 @@ import lombok.Data;
 public class LoginResponse {
     private Integer userId;
     private String roleName;
-    private String token;
-
-//    public LoginResponse(String token, String email, String roleName) {
-//        this.token = token;
-//        this.email =
-//
-//    }
-
 }
